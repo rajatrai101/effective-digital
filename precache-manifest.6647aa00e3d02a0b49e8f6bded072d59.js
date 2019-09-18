d9326dcd719b62e9ed3c12a204122c83c305ff43
Rajat Rai <rajatrai1206@gmail.com>
2019-09-19 01:05:57 +0530

    Updates

---
+++ precache-manifest.6647aa00e3d02a0b49e8f6bded072d59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7812418c1c09dab21a27a0a3389e2aeb",
+    "url": "/rajatrai101/effective-digital/index.html"
+  },
+  {
+    "revision": "fa1e5ef85544b728e0a9",
+    "url": "/rajatrai101/effective-digital/static/css/main.94416350.chunk.css"
+  },
+  {
+    "revision": "5a8cfbdf492dd0d2e912",
+    "url": "/rajatrai101/effective-digital/static/js/2.1320aebf.chunk.js"
+  },
+  {
+    "revision": "fa1e5ef85544b728e0a9",
+    "url": "/rajatrai101/effective-digital/static/js/main.8a4c9c87.chunk.js"
+  },
+  {
+    "revision": "e54f1b3cc396cdd176c1",
+    "url": "/rajatrai101/effective-digital/static/js/runtime~main.2b1c7fd1.js"
+  }
+]);
